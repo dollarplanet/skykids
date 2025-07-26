55414c20f256d22209f4a4483a5bb79843404b46
dollarplanet <agusriyanto.artist@gmail.com>
2025-07-26 23:42:36 +0700

    preview

--- src/Footer/Component.tsx
+++ src/Footer/Component.tsx
@@ -3,9 +3,6 @@ import Link from 'next/link'
 import React from 'react'
 
 import type { Footer } from '@/payload-types'
-
-import { ThemeSelector } from '@/providers/Theme/ThemeSelector'
-import { CMSLink } from '@/components/Link'
 import { Logo } from '@/components/Logo/Logo'
 
 export async function Footer() {
@@ -20,14 +17,14 @@ export async function Footer() {
           <Logo />
         </Link>
 
-        <div className="flex flex-col-reverse items-start md:flex-row gap-4 md:items-center">
+        {/* <div className="flex flex-col-reverse items-start md:flex-row gap-4 md:items-center">
           <ThemeSelector />
           <nav className="flex flex-col md:flex-row gap-4">
             {navItems.map(({ link }, i) => {
               return <CMSLink className="text-white" key={i} {...link} />
             })}
           </nav>
-        </div>
+        </div> */}
       </div>
     </footer>
   )
---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20250726_155435 from './20250726_155435';
+
+export const migrations = [
+  {
+    up: migration_20250726_155435.up,
+    down: migration_20250726_155435.down,
+    name: '20250726_155435'
+  },
+];
