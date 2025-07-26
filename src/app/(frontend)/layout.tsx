ce984faba254a6ae5fea366d5bea5c47d2f48be2
dollarplanet <agusriyanto.artist@gmail.com>
2025-07-27 01:39:48 +0700

    disable admin nav

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -27,11 +27,11 @@ export default async function RootLayout({ children }: { children: React.ReactNo
       </head>
       <body>
         <Providers>
-          <AdminBar
+          {/* <AdminBar
             adminBarProps={{
               preview: isEnabled,
             }}
-          />
+          /> */}
 
           <Header />
           {children}
